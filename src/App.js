e0f2fe9143c6b51a163e0757fe82b64f22e4106c
sunil kumar sahoo <sunil.sahoo@sumerusolutions.com>
2020-12-28 19:46:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.spacexdata.com/v3/launches?limit=100")
+      .then((response) => {
+        setApiData(response.data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h4 className="main-heading">SpaceX Launch Programs</h4>
+      <div className="cards">
+        {apiData && apiData.length > 0
+          ? apiData.map((data) => {
+              return (
+                <div className="card">
+                  <picture className="thumbnail">
+                    <img src={data.links.mission_patch_small} />
+                  </picture>
+                  <div className="card-content">
+                    <h2>
+                      <a href="#">{`${data.mission_name}#${data.flight_number}`}</a>
+                    </h2>
+                    <ul>
+                      <li>
+                        <div>Mission Ids : </div>
+                        <div className="display-value"></div>
+                      </li>
+                      <li>
+                        <div>Launch Year : </div>
+                        <div className="display-value">{data.launch_year}</div>
+                      </li>
+                      <li>
+                        <div>Successful Launch : </div>
+                        <div className="display-value">
+                          {data.launch_success ? "true" : "false"}
+                        </div>
+                      </li>
+                      <li>
+                        <div>Successful Landing : </div>
+                        <div className="display-value"></div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              );
+            })
+          : ""}
+      </div>
     </div>
   );
 }
